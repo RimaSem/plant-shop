27100c6e021de0c6b82d1c85acf0064c1b26c5f5
RimaSem <rima.semetaite@gmail.com>
2023-03-03 14:57:36 +0200

    Update hero section for all screen sizes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,12 +4,12 @@ function Hero() {
   return (
     <div className="hero-container">
       <h1 className="hero-title">
-        Perfect <span className="hero-title-highlighted">Plants</span> for your
-        space
+        Perfect <span className="hero-title-highlighted">Plants</span> for Your
+        Space
       </h1>
       <p>
         Get healthy house plants, succulents and air plants shipped directly
-        from our greenhouse to your door
+        from our plant shop to your door.
       </p>
       <button type="button">FIND YOUR PLANT</button>
     </div>
