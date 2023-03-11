65587e6c08e0216f79d578f9a583339dc1bed515
RimaSem <rima.semetaite@gmail.com>
2023-03-11 15:59:05 +0200

    Modify data file

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,30 @@
+import MarbleQueen from "/marble_queen.jpg";
+import CylSnakePlant from "/cylindrical_snake_plant.jpg";
+import BostonFern from "/boston-fern.jpg";
+import Haworthia from "/succulent-haworthia.jpg";
+import ParlorPalm from "/parlor-palm.jpg";
+import PeaceLily from "/peace-lily.jpg";
+import SpiderPlant from "/curly-spider-plant.jpg";
+import CornPlant from "/corn-plant.jpg";
+import SnakePlant from "/snake-plant.jpg";
+import Aspidistra from "/aspidistra.jpg";
+import Monstera from "/monstera.jpg";
+import PrayerPlant from "/maranta.jpg";
+import Peperomia from "/peperomia.jpg";
+import MoneyPlant from "/money-plant.jpg";
+import Ficus from "/ficus-ginseng.jpg";
+import ZZPlant from "/zamioculcas.jpg";
+import Philodendron from "/philodendron.jpg";
+import FanPalm from "/dwarf-fan-palm.jpg";
+import Calathea from "/calathea.jpg";
+import MaidenhairFern from "/maidenhair-fern.jpg";
+
 export default [
   {
     id: 1,
     name: "Marble Queen Pothos",
     price: 18,
-    img: "/marble_queen.jpg",
+    img: MarbleQueen,
     description:
       "Marble Queen, or Devil's Ivy, is a tropical, climbing plant with origins in the Solomon Islands. The Marble Queen is a variegated cultivar of Epipremnum aureum. Its waxy green leaves have streaks and patches of white, creating a bright and striking foliage plant. Marble Queens have oval leaves with heart shaped bases.",
     isFavorite: false,
@@ -15,7 +36,7 @@ export default [
     id: 2,
     name: "Cylindrical Snake Plant",
     price: 15,
-    img: "/cylindrical_snake_plant.jpg",
+    img: CylSnakePlant,
     description:
       "Cylindrical Snake Plant is an African succulent that makes a carefree house plant.Round leaves with a dark-green striped pattern give this eye-catching succulent its common name. Pointed leaf tips give it another name, African Spear (Watch out for those points -- they are sharp!). The gray-green tubular leaves grow in a rosette and are about an inch thick. Wipe leaves with a damp cloth to keep them dust-free.",
     isFavorite: false,
@@ -27,7 +48,7 @@ export default [
     id: 3,
     name: "Boston Fern",
     price: 12,
-    img: "/boston-fern.jpg",
+    img: BostonFern,
     description:
       "The Boston Fern has graceful green, drooping fronds that are naturally cut in such a way to give a ruffled looking effect. They're quite sensitive to chemicals and the smoke from coal fires or wood burners are fairly toxic, draughts must also be avoided. This means your choice of placement will need to be considered quite carefully if your home has any of these features.",
     isFavorite: false,
@@ -39,7 +60,7 @@ export default [
     id: 4,
     name: "Haworthia",
     price: 15,
-    img: "/succulent-haworthia.jpg",
+    img: Haworthia,
     description:
       "Haworthia is a large genus of small succulent plants, most of them native to South Africa. They are generally lumped under the common name haworthia, though different species might carry other common names. These plants are delightful little succulents that make attractive small houseplants",
     isFavorite: false,
@@ -50,7 +71,7 @@ export default [
     id: 5,
     name: "Parlor Palm",
     price: 9,
-    img: "/parlor-palm.jpg",
+    img: ParlorPalm,
     description:
       "The parlor palm (Chamaedorea elegans) has long been used as an indoor plant. This lovely plant with deep green foliage was first discovered in Central America and brought back to the United States where it immediately became a popular indoor palm. It grows in attractive clumps with light-textured foliage cloaking thin trunks.",
     isFavorite: false,
@@ -62,7 +83,7 @@ export default [
     id: 6,
     name: "Peace Lily",
     price: 15,
-    img: "/peace-lily.jpg",
+    img: PeaceLily,
     description:
       "The peace lily is a flowering tropical plant from the Spathiphyllum genus. Outside of its hardiness zones, many people grow the peace lily indoors. There are many types of peace lilies within the genus. But in general, peace lilies have large, glossy, oval leaves. They typically bloom in the spring, though that can vary when growing them as houseplants.",
     isFavorite: false,
@@ -73,7 +94,7 @@ export default [
     id: 7,
     name: "Curly Spider Plant",
     price: 27,
-    img: "/curly-spider-plant.jpg",
+    img: SpiderPlant,
     description:
       "Despite their creepy-crawly name, spider plants (Chlorophytum comosum) are among the most popular houseplants to grow. When grown indoors, these warm-weather perennials will survive less-than-perfect conditions, and they are stunning if you can closely mimic their native tropical environment by providing warm temperatures and humid air.",
     isFavorite: false,
@@ -84,7 +105,7 @@ export default [
     id: 8,
     name: "Corn Plant",
     price: 35,
-    img: "/corn-plant.jpg",
+    img: CornPlant,
     description:
       "The corn plant (Dracaena fragrans) is a tropical African evergreen tree popular in Europe as an indoor plant since the mid-1800s—and in the U.S. since the early 20th century. They grow fairly slowly, from thick canes or stems that produce long, narrow leaves like stalks of corn, growing upward.",
     isFavorite: false,
@@ -96,7 +117,7 @@ export default [
     id: 9,
     name: "Snake Plant",
     price: 20,
-    img: "/snake-plant.jpg",
+    img: SnakePlant,
     description:
       "Dracaena trifasciata, commonly known as the snake plant, is one of the most popular and hardy species of houseplants. Up until 2017, it was botanically classified as Sansevieria trifasciata, but its commonalities with Dracaena species were too many to overlook. The plant features stiff, sword-like leaves and can range anywhere from six inches to eight feet tall.",
     isFavorite: false,
@@ -107,7 +128,7 @@ export default [
     id: 10,
     name: "Cast-Iron Plant",
     price: 27,
-    img: "/aspidistra.jpg",
+    img: Aspidistra,
     description:
       "The cast-iron plant (Aspidistra elatior) has earned its reputation as a hard-to-kill houseplant, along with being a beautiful outdoor foliage plant within its growing zones. This plant can survive lots of neglect and growing conditions that will kill many other plants, such as low light conditions. It has arching, lance-shaped, deep green, glossy leaves that can reach around 2 feet long and 4 inches wide.",
     isFavorite: false,
@@ -118,7 +139,7 @@ export default [
     id: 11,
     name: "Monstera",
     price: 35,
-    img: "/monstera.jpg",
+    img: Monstera,
     description:
       "Native to the rainforests of Central America, the big, bold Monstera deliciosa plant is also known as the split-leaf philodendron. This easy-to-grow climbing evergreen can be found in many designer spaces for its wow factor. Indoors, the plant has a moderate growth rate and can grow in height about 1 to 2 feet a year.",
     isFavorite: false,
@@ -130,7 +151,7 @@ export default [
     id: 12,
     name: "Prayer Plant",
     price: 20,
-    img: "/maranta.jpg",
+    img: PrayerPlant,
     description:
       "The prayer plant is one of the most distinguishable tropicals, thanks to its beautiful decorative leaves. The popular tricolor variety has deep green, velvety leaves with yellow splotches down the midrib and arching red veins traveling to the leaf margins. A slow-grower, the prayer plant can eventually reach up to a foot in height indoors.",
     isFavorite: false,
@@ -141,7 +162,7 @@ export default [
     id: 13,
     name: "Peperomia",
     price: 13,
-    img: "/peperomia.jpg",
+    img: Peperomia,
     description:
       "Peperomia (Peperomia spp.) is a genus of tropical plants from Central and South America containing more than 1,500 species, with the common types often being grown as compact houseplants. While many peperomia plants have bright green leaves, the foliage can come in different colors, textures, and patterns. In general, they make for slow-growing and low-maintenance plants. ",
     isFavorite: false,
@@ -152,7 +173,7 @@ export default [
     id: 14,
     name: "Chinese Money Plant",
     price: 16,
-    img: "/money-plant.jpg",
+    img: MoneyPlant,
     description:
       "Pilea peperomioides, often referred to as the pancake plant, Chinese money plant, coin plant, or UFO plant is a popular houseplant thanks to its attractive coin-shaped foliage and ease of care. This flowering perennial in the nettle family (Urticaceae) is native to southern China, growing naturally along the base of the Himalayan mountains.",
     isFavorite: false,
@@ -164,7 +185,7 @@ export default [
     id: 15,
     name: "Ginseng Ficus",
     price: 44,
-    img: "/ficus-ginseng.jpg",
+    img: Ficus,
     description:
       "If you are looking for an entry into the ancient Japanese art of Bonsai, a great place to start is with a Ginseng Ficus. It looks terrific and requires minimal care when compared to other bonsai, which can be quite difficult to maintain. Bonsai artists especially focus on the raised roots of these funky little plants to create a certain aesthetic.",
     isFavorite: false,
@@ -175,7 +196,7 @@ export default [
     id: 16,
     name: "ZZ Plant",
     price: 31,
-    img: "/zamioculcas.jpg",
+    img: ZZPlant,
     description:
       "Low-maintenance zz plants are Zamioculcas zamiifolia, characterized by their shiny, wide, oval-shaped leaves that shoot upward and quickly grow in a home indoors. The plant natively grows in East Africa, namely Zanzibar and Tanzania. The plants are also called Zanzibar gems for their spotless, waxy leaves that are so deep green that sometimes, these plants are mistaken as artificial.",
     isFavorite: false,
@@ -186,7 +207,7 @@ export default [
     id: 17,
     name: "Philodendron",
     price: 18,
-    img: "/philodendron.jpg",
+    img: Philodendron,
     description:
       "There are hundreds of philodendron species, and these popular houseplants are beloved for their easy-going nature and large, glossy-green foliage. Slow-growing and compact, it's a great plant for small spaces, providing it gets enough bright, indirect light to promote the eye-catching variegation.",
     isFavorite: false,
@@ -198,7 +219,7 @@ export default [
     id: 18,
     name: "Dwarf Fan Palm",
     price: 38,
-    img: "/dwarf-fan-palm.jpg",
+    img: FanPalm,
     description:
       "The dwarf fan palm is an especially cold-hardy species of palm tree. It can tolerate temperatures as low as 10 degrees Fahrenheit. For this reason, dwarf fan palms are frequently used to add tropical flair to temperate landscapes. They also grow well as houseplants, adapting easily to indoor conditions.",
     isFavorite: false,
@@ -209,7 +230,7 @@ export default [
     id: 19,
     name: "Calathea",
     price: 20,
-    img: "/calathea.jpg",
+    img: Calathea,
     description:
       "The genus Calathea—characterized by boldly marked, oblong leaves in a dazzling array of colors—includes some of the most beautiful tropical plants in the world. Due to the plants' eye-catching stripes and veining, they're often known by nicknames such as the zebra plant, peacock plant, or rattlesnake plant. Calatheas are also referred to as prayer plants, a common name that other species use as well.",
     isFavorite: false,
@@ -220,7 +241,7 @@ export default [
     id: 20,
     name: "Maidenhair Fern",
     price: 20,
-    img: "/maidenhair-fern.jpg",
+    img: MaidenhairFern,
     description:
       "Maidenhair ferns have delicate fan-shaped leaf segments, typically clustered on wiry black stems, and their leaves are smaller than other types of ferns. In addition to being one of the most popular fern houseplants, the maidenhair fern can also be found in nature, growing in places where other plants typically don't, like on rock walls and in between rock fissures where the moisture from water seepage keeps them alive.",
     isFavorite: false,
